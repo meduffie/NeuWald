c76cea919004386e8b9c960dfa921ec7cf010319
Austin Duffie <meduffie@gmail.com>
2015-03-30 17:11:52 +0000

    ~

--- src/World.java
+++ src/World.java
@@ -10,18 +10,23 @@ public class World {
     //False & Null = No exit in that direction, see constructor
     //True & Null = Waiting for relationship (Method setRelations), called in Client
     Room Spawn = new Room("Spawn", "This is a dark room with one exit.",
-    true, null, false, null, false, null, false, null, 1,0);
+    true, null, false, null, false, null, false, null, 0,0);
     
     String gd = "Welcome to the New World, this is a grassy plain. The sun shines brightly in the summer sky, and you feel a great warmth cascade over you.";
-    Room Grass = new Room("Grass", gd , false, null, false, null, true, null, true, null, 2,0); 
+    Room Grass = new Room("Grass", gd , true, null, false, null, true, null, true, null, 0,1); 
     
     Room Forrest = new Room("Forrest", "The smell of fresh Oak fills the air, this seems to be a forrest of some sort",
-    false, null, true, null, false, null, false, null, 2,1);
+    false, null, true, null, false, null, false, null, 1,1);
+    
+    Room Prarie = new Room("Prarie", "The wheat leaves rumble in the air, and there seems to be nothing but prarie grass for miles",
+    false, null, false, null, true, null, false, null, 0,2);
     
     public void setWorld(){
         Spawn.setExitN(Grass);
         Grass.setExitS(Spawn);
         Grass.setExitW(Forrest);
+        Grass.setExitN(Prarie);
+        Prarie.setExitS(Grass);
         Forrest.setExitE(Grass);
         Forrest.addI(bread);
         Forrest.addI(wSword);
